c7c0be9f90f21c682de4d0ee4d4ff19286ffc09d
Faysal Regal <faysalregal@gmail.com>
2022-08-21 21:36:29 +0100

    started exercise 4

--- Program.cs
+++ Program.cs
@@ -1,6 +1,23 @@
-﻿    //Exercise 4
-    //Write a program and ask the user to enter a few words separated by a space. 
-    //Use the words to create a variable name with PascalCase. 
-    //For example, if the user types: "number of students", display "NumberOfStudents".
-    //Make sure that the program is not dependent on the input. 
-    //So, if the user types "NUMBER OF STUDENTS", the program should still display "NumberOfStudents".
+﻿//Exercise 4
+//Write a program and ask the user to enter a few words separated by a space. 
+//Use the words to create a variable name with PascalCase. 
+//For example, if the user types: "number of students", display "NumberOfStudents".
+//Make sure that the program is not dependent on the input. 
+//So, if the user types "NUMBER OF STUDENTS", the program should still display "NumberOfStudents".
+
+Console.Write("Please enter a few words separated by a space: ");
+
+var userInput = Console.ReadLine();
+var splitInput = userInput.Split(' ');
+
+
+
+for (var i = 0; i < splitInput.Length; i++)
+{
+    String.ToUpper(splitInput[0]);
+}
+
+foreach (var item in splitInput)
+{
+    Console.WriteLine(item);
+}
