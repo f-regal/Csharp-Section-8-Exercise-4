b6372c25c8dded4b6bcc92cff18d2043b2517858
Faysal Regal <faysalregal@gmail.com>
2022-08-22 18:16:47 +0100

    completed exercise 4

--- Program.cs
+++ Program.cs
@@ -4,20 +4,30 @@
 //For example, if the user types: "number of students", display "NumberOfStudents".
 //Make sure that the program is not dependent on the input. 
 //So, if the user types "NUMBER OF STUDENTS", the program should still display "NumberOfStudents".
-
-Console.Write("Please enter a few words separated by a space: ");
-
-var userInput = Console.ReadLine();
-var splitInput = userInput.Split(' ');
+using System;
 
 
 
-for (var i = 0; i < splitInput.Length; i++)
+namespace CsharpFundamentals
 {
-    String.ToUpper(splitInput[0]);
-}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a few words separated by a space: ");
 
-foreach (var item in splitInput)
-{
-    Console.WriteLine(item);
+            var userInput = Console.ReadLine();
+            var splitInput = userInput.Split(' ');
+            List<string> newSentenceArr = new List<string>();
+
+            for (var i = 0; i < splitInput.Length; i++)
+            {
+                newSentenceArr.Add(splitInput[i][0].ToString().ToUpper() + splitInput[i].Substring(1, splitInput[i].Length - 1).ToLower());
+            }
+
+            var sentence = string.Join("", newSentenceArr);
+            Console.WriteLine(sentence);
+
+        }
+    }
 }
